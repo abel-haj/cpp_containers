12312850db8095a11132a41d4e54a2af5f3bba4c
abel-haj <abel-haj@student.1337.ma>
2022-07-02 00:12:44 +0100

    added namespace in inclusion guard

--- other/enable_if.hpp
+++ other/enable_if.hpp
@@ -1,10 +1,19 @@
-template<bool condition, class T = void>
-class enable_if
-{};
+#ifndef		ENABLE_IF_HPP
+# define	ENABLE_IF_HPP
 
-template<class T>
-class enable_if<true, T>
+namespace ft
 {
-	public:
-		typedef T type;
+	template<bool condition, class T = void>
+	class enable_if
+	{};
+
+	template<class T>
+	class enable_if<true, T>
+	{
+		public:
+			typedef T type;
+	};
+
 };
+
+#endif
--- other/is_integral.hpp
+++ other/is_integral.hpp
@@ -1,20 +1,30 @@
-template <class T>
-class is_integral
+#ifndef		IS_INTEGRAL_HPP
+# define	IS_INTEGRAL_HPP
+
+namespace ft
 {
-	public:
-		static const T value = false;
+
+	template <class T>
+	class is_integral
+	{
+		public:
+			static const T value = false;
+	};
+
+	template <>
+	class is_integral<const int>
+	{
+		public:
+			static const int value = true;
+	};
+
+	template <>
+	class is_integral<int>
+	{
+		public:
+			static const int value = true;
+	};
+
 };
 
-template <>
-class is_integral<const int>
-{
-	public:
-		static const int value = true;
-};
-
-template <>
-class is_integral<int>
-{
-	public:
-		static const int value = true;
-};
+#endif
