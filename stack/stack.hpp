623da60e51da635554cd02fec26b34c800cdf71d
abel-haj <abel-haj@student.1337.ma>
2022-07-23 01:11:18 +0100

    first implementation of stack

---
+++ stack/stack.hpp
@@ -0,0 +1,105 @@
+#ifndef		STACK_HPP
+# define	STACK_HPP
+
+
+namespace ft {
+
+	# include "../vector/vector.hpp"
+
+	template <class T,
+		class Container = ft::vector<T>
+	>
+	class stack {
+
+		private:
+
+		public:
+			typedef T									value_type;
+			typedef Container							container_type;
+			typedef typename container_type::size_type	size_type;
+
+		protected:
+			container_type								ct;
+
+		public:
+			explicit stack (const container_type& ctnr = container_type())
+			{
+				ct = ctnr;
+			}
+
+			bool empty() const
+			{
+				return ct.empty();
+			}
+
+			void pop()
+			{
+				ct.pop_back();
+			}
+
+			void push (const value_type& val)
+			{
+				ct.push_back(val);
+			}
+
+			size_type size() const
+			{
+				return ct.size();
+			}
+
+			value_type& top()
+			{
+				ct.back();
+			}
+			const value_type& top() const
+			{
+				ct.back();
+			}
+
+			// (1)
+			template <class T, class Container>
+			friend bool operator== (const stack<T,Container>& lhs, const stack<T,Container>& rhs)
+			{
+				return lhs.ct == rhs.ct;
+			}
+
+			// (2)
+			template <class T, class Container>
+			friend bool operator!= (const stack<T,Container>& lhs, const stack<T,Container>& rhs)
+			{
+				return lhs.ct != rhs.ct;
+			}
+
+			// (3)
+			template <class T, class Container>
+			friend bool operator<  (const stack<T,Container>& lhs, const stack<T,Container>& rhs)
+			{
+				return lhs.ct < rhs.ct;
+			}
+
+			// (4)
+			template <class T, class Container>
+			friend bool operator<= (const stack<T,Container>& lhs, const stack<T,Container>& rhs)
+			{
+				return lhs.ct <= rhs.ct;
+			}
+
+			// (5)
+			template <class T, class Container>
+			friend bool operator>  (const stack<T,Container>& lhs, const stack<T,Container>& rhs)
+			{
+				return lhs.ct > rhs.ct;
+			}
+
+			// (6)
+			template <class T, class Container>
+			friend bool operator>= (const stack<T,Container>& lhs, const stack<T,Container>& rhs)
+			{
+				return lhs.ct >= rhs.ct;
+			}
+
+	};
+
+};
+
+#endif
