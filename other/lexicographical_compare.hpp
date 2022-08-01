42b4f5fed8500c957e48956df3b05e417fc11684
abel-haj <abel-haj@student.1337.ma>
2022-08-02 00:23:11 +0100

    finished equal and lexographical compare implementations

---
+++ other/lexicographical_compare.hpp
@@ -0,0 +1,50 @@
+#ifndef		FT_LEXICOGRAPHICAL_COMPARE
+# define	FT_LEXICOGRAPHICAL_COMPARE
+
+namespace ft {
+
+	// default (1)	
+	template <class InputIterator1, class InputIterator2>
+	bool lexicographical_compare (InputIterator1 first1, InputIterator1 last1,
+									InputIterator2 first2, InputIterator2 last2)
+									{
+										while (first1 != last1)
+										{
+											if (first2 == last2)
+												return false;
+
+											if (*first1 < *first2)
+												return true;
+											if (*first1 > *first2)
+												return false;
+
+											first1++;
+											first2++;
+										}
+										return false;
+									}
+	// custom (2)	
+	template <class InputIterator1, class InputIterator2, class Compare>
+	bool lexicographical_compare (InputIterator1 first1, InputIterator1 last1,
+									InputIterator2 first2, InputIterator2 last2,
+									Compare comp)
+									{
+										while (first1 != last1)
+										{
+											if (first2 == last2)
+												return false;
+
+											if ( comp(*first1, *first2) )
+												return true;
+											if ( comp(*first2, *first1) )
+												return false;
+
+											first1++;
+											first2++;
+										}
+										return false;
+									}
+
+};
+
+#endif
