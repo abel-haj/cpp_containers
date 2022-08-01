736d05bf607e2611927c2e2d5cfa1c386010525e
abel-haj <abel-haj@student.1337.ma>
2022-08-02 00:25:17 +0100

    fixed nested instance problem

--- stack/stack.hpp
+++ stack/stack.hpp
@@ -1,10 +1,9 @@
 #ifndef		STACK_HPP
 # define	STACK_HPP
 
+# include "../vector/vector.hpp"
 
 namespace ft {
-
-	# include "../vector/vector.hpp"
 
 	template <class T,
 		class Container = ft::vector<T>
