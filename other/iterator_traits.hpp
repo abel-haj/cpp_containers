c1f46b3d9dcf27139df3799e9bbcb1b2ad013e19
abel-haj <abel-haj@student.1337.ma>
2022-08-02 04:27:21 +0100

    private -> public

--- other/iterator_traits.hpp
+++ other/iterator_traits.hpp
@@ -6,6 +6,7 @@ namespace ft {
 	template <class Iterator>
 	class iterator_traits {
 
+		public:
 		typedef typename Iterator::difference_type		difference_type;
 		typedef typename Iterator::value_type			value_type;
 		typedef typename Iterator::pointer				pointer;
@@ -17,22 +18,24 @@ namespace ft {
 	template <class T>
 	class iterator_traits<T*> {
 
+		public:
 		typedef ptrdiff_t									difference_type;
 		typedef T											value_type;
 		typedef T*											pointer;
 		typedef T&											reference;
-		typedef std::random_access_iterator_tag	iterator_category;
+		typedef std::random_access_iterator_tag				iterator_category;
 
 	};
 
 	template <class T>
 	class iterator_traits<const T*> {
 
+		public:
 		typedef ptrdiff_t									difference_type;
 		typedef T											value_type;
 		typedef const T*									pointer;
 		typedef const T&									reference;
-		typedef std::random_access_iterator_tag	iterator_category;
+		typedef std::random_access_iterator_tag				iterator_category;
 
 	};
 
