f9d847d0c28608a7470946c3147bc018fdc8cf82
abel-haj <abel-haj@student.1337.ma>
2022-08-20 07:47:07 +0100

    working on destructor and freeing memory, finished bool return of insert and size return of erase

--- map/base_map.hpp
+++ map/base_map.hpp
@@ -26,45 +26,62 @@ namespace ft {
 			typedef				size_t										size_type;
 
 		private:
-			size_type								_size;
 			key_compare								_comp_func;
 			node_type *								_top;
 			std::allocator< Avl_Node<value_type> >	_alloc_node;
 
 		public:
-			void erase (const key_type& k)
+			avl_tree () : _top(NULL) {}
+			~avl_tree ()
 			{
-				erase_wrap(_top, k);
+				if (_top)
+					delete_tree(_top);
 			}
-			node_type * erase_wrap (node_type *& pos, const key_type& k)
+
+			void delete_tree(node_type * pos)
 			{
+				if (pos->left)
+					delete_tree(pos->left);
+				if (pos->right)
+					delete_tree(pos->right);
+
+				_alloc_node.deallocate(pos, 1);
+			}
+
+			size_type erase (const key_type& k)
+			{
+				return erase_wrap(_top, k);
+			}
+			size_type erase_wrap (node_type *& pos, const key_type& k)
+			{
+				size_type ret = 0;
+
 				if (pos)
 				{
 				// Smaller
 					if ( _comp_func(k, pos->current->first) )
-						pos->left = erase_wrap(pos->left, k);
+						ret = erase_wrap(pos->left, k);
 
 					// Greater
 					else if ( _comp_func(pos->current->first, k) )
-						pos->right = erase_wrap(pos->right, k);
+						ret = erase_wrap(pos->right, k);
 
 					// Same
 					else
 					{
 						if ( pos->left == NULL && pos->right == NULL )
 						{
-							std::cout << "LR" << std::endl;
+							// std::cout << "LR" << std::endl;
 							_alloc_node.deallocate(pos, 1);
 							// _alloc_node.destroy(pos);
 							pos = NULL;
 
-							return (NULL);
-
+							return 1;
 						}
 
 						else if ( pos->left == NULL )
 						{
-							std::cout << "L" << std::endl;
+							// std::cout << "L" << std::endl;
 							node_type * tmp = pos->right;
 							node_type * tmp_p = pos->parent;
 
@@ -74,11 +91,12 @@ namespace ft {
 
 							pos = tmp;
 							tmp->parent = tmp_p;
+							ret = 1;
 						}
 
 						else if ( pos->right == NULL )
 						{
-							std::cout << "R" << std::endl;
+							// std::cout << "R" << std::endl;
 							node_type * tmp = pos->left;
 							node_type * tmp_p = pos->parent;
 
@@ -88,19 +106,20 @@ namespace ft {
 
 							pos = tmp;
 							tmp->parent = tmp_p;
+							ret = 1;
 						}
 
 						else
 						{
-							std::cout << "T3" << std::endl;
+							// std::cout << "T3" << std::endl;
 							node_type * tmp = deepest_left(pos->right);
 							pos->current = tmp->current;
-							pos->right = erase_wrap(pos->right, tmp->current->first);
+							ret = erase_wrap(pos->right, tmp->current->first);
 						}
 					}
 
 					if (!pos)
-						return pos;
+						return ret;
 
 					pos->height = 1 + std::max(get_height(pos->left), get_height(pos->right));
 
@@ -120,9 +139,153 @@ namespace ft {
 
 					else if (this_balance < -1 && get_balance_factor(pos->right) > 0)
 						right_left_rotation(pos);
-
 				}
-				return pos;
+				return ret;
+			}
+
+			bool insert (const value_type& val)
+			{
+				return insert_wrap(_top, val, NULL);
+				return insert_wrap_2(_top, val);
+			}
+			bool insert_wrap (node_type *& pos, const value_type& val, node_type * p)
+			{
+				node_type	new_node(val);
+				int	ret = false;
+
+				if ( pos == NULL )
+				{
+					pos = _alloc_node.allocate(1);
+					pos->parent = p;
+					_alloc_node.construct(pos, val);
+					return true;
+				}
+
+				else
+				{
+					if ( _comp_func(pos->current->first, val.first) )
+						ret = insert_wrap(pos->right, val, pos);
+					else if ( _comp_func(val.first, pos->current->first) )
+						ret = insert_wrap(pos->left, val, pos);
+					else
+					{
+						// replace value
+						pos->current->second = val.second;
+						return false;
+					}
+					if (ret == false)
+						return false;
+
+					pos->height = 1 + std::max(get_height(pos->left), get_height(pos->right));
+
+					int this_balance = get_balance_factor(pos);
+
+					// LEFT LEFT : RIGHT ROTATION
+					if (this_balance > 1 && get_balance_factor(pos->left) > 0)
+						right_rotation(pos);
+
+					// RIGHT RIGHT : LEFT ROTATION
+					else if (this_balance < -1 && get_balance_factor(pos->right) < 0)
+						left_rotation(pos);
+
+					// LEFT RIGHT
+					else if (this_balance > 1 && get_balance_factor(pos->left) < 0)
+						left_right_rotation(pos);
+
+					else if (this_balance < -1 && get_balance_factor(pos->right) > 0)
+						right_left_rotation(pos);
+				}
+
+				return ret;
+			}
+			bool insert_wrap_2 (node_type *& pos, const value_type& val)
+			{
+				// bool cond;
+				int orient = 0;
+				node_type * p = NULL;
+				node_type * tmp_node = pos;
+
+				// SEARCH
+				while (tmp_node)
+				{
+					if ( _comp_func(tmp_node->current->first, val.first) )
+					{
+						orient = 1;
+						p = tmp_node;
+						tmp_node = tmp_node->right;
+					}
+					else if ( _comp_func(val.first, tmp_node->current->first) )
+					{
+						orient = 2;
+						p = tmp_node;
+						tmp_node = tmp_node->left;
+					}
+					else
+					{
+						tmp_node->current->second = val.second;
+						return false;
+					}
+				}
+
+				// INSERT
+				if (tmp_node == NULL)
+				{
+					tmp_node = _alloc_node.allocate(1);
+					_alloc_node.construct(tmp_node, val);
+					tmp_node->parent = p;
+					if (orient == 1)
+						p->right = tmp_node;
+					if (orient == 2)
+						p->left = tmp_node;
+
+					// editing top reference
+					if (pos == NULL)
+						pos = tmp_node;
+				}
+
+				// BALANCE
+				while (tmp_node)
+				{
+					p = tmp_node;
+					balance_tree(tmp_node);
+					tmp_node = tmp_node->parent;
+				}
+
+				// update top reference
+				pos = p;
+
+				return true;
+			}
+			void balance_tree(node_type *& pos)
+			{
+				// if (pos->current->first == 4)
+				// 	std::cout << "height " << pos->height << std::endl;
+
+				pos->height = 1 + std::max(get_height(pos->left), get_height(pos->right));
+				// if (pos->current->first == 4)
+				// 	std::cout << "height " << pos->height << std::endl;
+				// 	std::cout << std::endl;
+
+				int this_balance = get_balance_factor(pos);
+
+				// std::cout << this_balance << " " <<
+				// get_balance_factor(pos->left) << " " <<
+				// get_balance_factor(pos->right) << std::endl;
+
+				// LEFT LEFT : RIGHT ROTATION
+				if (this_balance > 1 && get_balance_factor(pos->left) > 0)
+					right_rotation(pos);
+
+				// RIGHT RIGHT : LEFT ROTATION
+				else if (this_balance < -1 && get_balance_factor(pos->right) < 0)
+					left_rotation(pos);
+
+				// LEFT RIGHT
+				else if (this_balance > 1 && get_balance_factor(pos->left) < 0)
+					left_right_rotation(pos);
+
+				else if (this_balance < -1 && get_balance_factor(pos->right) > 0)
+					right_left_rotation(pos);
 			}
 
 			node_type * deepest_left(node_type * n)
@@ -138,54 +301,6 @@ namespace ft {
 					while (n->right != NULL)
 						n = n->right;
 				return n;
-			}
-
-			void insert (const value_type& val)
-			{
-				insert_wrap(_top, val, NULL);
-			}
-			void insert_wrap (node_type *& pos, const value_type& val, node_type * p)
-			{
-				node_type	new_node(val);
-
-				if ( pos == NULL )
-				{
-					pos = _alloc_node.allocate(1);
-					pos->parent = p;
-					_alloc_node.construct(pos, val);
-				}
-
-				else
-				{
-					if ( _comp_func(pos->current->first, val.first) )
-						insert_wrap(pos->right, val, pos);
-					else if ( _comp_func(val.first, pos->current->first) )
-						insert_wrap(pos->left, val, pos);
-					else
-					{
-						// replace value
-					}
-						
-					pos->height = 1 + std::max(get_height(pos->left), get_height(pos->right));
-
-					int this_balance = get_balance_factor(pos);
-
-					// LEFT LEFT : RIGHT ROTATION
-					if (this_balance > 1 && get_balance_factor(pos->left) > 0)
-						right_rotation(pos);
-
-					// RIGHT RIGHT : LEFT ROTATION
-					else if (this_balance < -1 && get_balance_factor(pos->right) < 0)
-						left_rotation(pos);
-
-					// LEFT RIGHT
-					else if (this_balance > 1 && get_balance_factor(pos->left) < 0)
-						left_right_rotation(pos);
-
-					else if (this_balance < -1 && get_balance_factor(pos->right) > 0)
-						right_left_rotation(pos);
-
-				}
 			}
 
 			void left_right_rotation(node_type * & n)
@@ -228,12 +343,20 @@ namespace ft {
 
 			void right_rotation(node_type * & n)
 			{
-				// POS PARENT ??
 				node_type * tmp_l = n->left;
-				n->left = tmp_l->right;
+				node_type * tmp_l_r = tmp_l->right;
+				node_type * tmp_p = n->parent;
+
 				tmp_l->right = n;
-				tmp_l->parent = NULL;
+				n->left = tmp_l_r;
+				n->parent = tmp_l;
 				n = tmp_l;
+
+				n->parent = tmp_p;
+				if (tmp_p)
+					tmp_p->right = n;
+				if (tmp_p)
+					std::cout << tmp_p->current->second << std::endl;
 
 				n->right->height = 1 + std::max(get_height(n->right->left), get_height(n->right->right));
 				n->left->height = 1 + std::max(get_height(n->left->left), get_height(n->left->right));
@@ -244,9 +367,18 @@ namespace ft {
 			{
 				node_type * tmp_r = n->right;
 				node_type * tmp_r_l = tmp_r->left;
+				node_type * tmp_p = n->parent;
+
 				tmp_r->left = n;
 				n->right = tmp_r_l;
+				n->parent = tmp_r;
 				n = tmp_r;
+
+				n->parent = tmp_p;
+				if (tmp_p)
+					tmp_p->left = n;
+				if (tmp_p)
+					std::cout << tmp_p->current->second << std::endl;
 
 				n->right->height = 1 + std::max(get_height(n->right->left), get_height(n->right->right));
 				n->left->height = 1 + std::max(get_height(n->left->left), get_height(n->left->right));
@@ -273,7 +405,7 @@ namespace ft {
 				std::cout << "------------" << std::endl;
 
 				printBT("", _top, false);
-				// check_balance(_top);
+				check_balance(_top);
 			}
 			void printBT(const std::string& prefix, const node_type* node, bool isLeft)
 			{
@@ -317,7 +449,6 @@ namespace ft {
 				}
 				return 1;
 			}
-
 			template<class F>
 			void check_balance(F *s)
 			{
