6cdda28ed5575d2e7e3b8613fd544b0f00fbc30f
abel-haj <abel-haj@student.1337.ma>
2022-08-20 07:52:46 +0100

    leaving this headache here for next time

--- map/bidirectional_iterator_map.hpp
+++ map/bidirectional_iterator_map.hpp
@@ -5,20 +5,36 @@
 
 namespace ft {
 
-	template <class Iterator>
+	template <class Tree, class Node, class T>
 	class map_iterator {
 
-		public:
-			typedef typename ft::map_iterator_traits<Iterator>::iterator_category	iterator_category;
-			typedef typename ft::map_iterator_traits<Iterator>::value_type			value_type;
-			typedef typename ft::map_iterator_traits<Iterator>::difference_type		difference_type;
-			typedef typename ft::map_iterator_traits<Iterator>::pointer				pointer;
-			typedef typename ft::map_iterator_traits<Iterator>::reference			reference;
+	public:
+		// typedef typename ft::map_iterator_traits<Iterator>::iterator_category	iterator_category;
+		// typedef typename ft::map_iterator_traits<Iterator>::value_type			value_type;
+		// typedef typename ft::map_iterator_traits<Iterator>::difference_type		difference_type;
+		// typedef typename ft::map_iterator_traits<Iterator>::pointer				pointer;
+		// typedef typename ft::map_iterator_traits<Iterator>::reference			reference;
 
-			map_iterator() {}
-			map_iterator(const map_iterator & ri);
-			map_iterator & operator=(const map_iterator & rhs);
-			~map_iterator() {}
+	private:
+		Tree _base;
+
+	public:
+		map_iterator() {}
+		map_iterator(const map_iterator & ri);
+		map_iterator & operator=(const map_iterator & rhs);
+		~map_iterator() {}
+
+		bool operator==(const map_iterator & rhs);
+		bool operator!=(const map_iterator & rhs);
+
+		// Iterator operator*(const map_iterator & rhs);
+		// reference operator*(const map_iterator & rhs) const;
+		Node& operator*(const map_iterator & rhs) const;
+		Node* operator->() const;
+		map_iterator operator--();
+		map_iterator operator--(int);
+		map_iterator operator++();
+		map_iterator operator++(int);
 
 	};
 
