c9013aabe8af842ca0b8a839e45d26f0641e2ba5
abel-haj <abel-haj@student.1337.ma>
2022-08-26 11:03:04 +0100

    more integrals

--- other/is_integral.hpp
+++ other/is_integral.hpp
@@ -5,25 +5,73 @@ namespace ft
 {
 
 	template <class T>
-	class is_integral
-	{
-		public:
-			static const int value = false;
-	};
+	struct is_integral
+	{ static const int value = false; };
 
 	template <>
-	class is_integral<const int>
-	{
-		public:
-			static const int value = true;
-	};
+	struct is_integral<bool>
+	{ static const int value = true; };
 
 	template <>
-	class is_integral<int>
-	{
-		public:
-			static const int value = true;
-	};
+	struct is_integral<char>
+	{ static const int value = true; };
+
+	template <>
+	struct is_integral<signed char>
+	{ static const int value = true; };
+
+	template <>
+	struct is_integral<unsigned char>
+	{ static const int value = true; };
+
+	template <>
+	struct is_integral<wchar_t>
+	{ static const int value = true; };
+
+	template <>
+	struct is_integral<short>
+	{ static const int value = true; };
+
+	template <>
+	struct is_integral<unsigned short>
+	{ static const int value = true; };
+
+	template <>
+	struct is_integral<int>
+	{ static const int value = true; };
+
+	template <>
+	struct is_integral<unsigned int>
+	{ static const int value = true; };
+
+	template <>
+	struct is_integral<long>
+	{ static const int value = true; };
+
+	template <>
+	struct is_integral<unsigned long>
+	{ static const int value = true; };
+
+	template <>
+	struct is_integral<long long>
+	{ static const int value = true; };
+
+	template <>
+	struct is_integral<unsigned long long>
+	{ static const int value = true; };
+
+
+	template <>
+	struct is_integral<float>
+	{ static const int value = true; };
+
+	template <>
+	struct is_integral<double>
+	{ static const int value = true; };
+
+	template <>
+	struct is_integral<long double>
+	{ static const int value = true; };
 
 };
 
